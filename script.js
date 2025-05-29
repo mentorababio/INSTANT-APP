f09890c16cfa3b92dd26b3deda8ec7d4e6e76382
EZE FRANCIS CHIDIEBERE <ritc971@gmail.com>
2025-05-29 02:51:03 +0100

    Add files via upload
    profile

---
+++ script.js
@@ -0,0 +1,29 @@
+function goBack() {
+    alert("Go back to previous screen");
+    
+  }
+
+  function editProfile() {
+    alert("Edit profile clicked");
+  }
+
+  function changePassword() {
+    alert("Change password clicked");
+  }
+
+  function openSupport() {
+    alert("Support clicked");
+  }
+
+  function logout() {
+    alert("Logging out...");
+  }
+  
+
+  function claimPoints() {
+    alert('Points claimed successfully!');
+  }
+
+  function goBack() {
+    window.history.back();
+  }
