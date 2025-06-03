e1c4dead8701627c493a5d3d4577efe4e7e1ca6e
fortunate okpata <fortunateokpata@gmail.com>
2025-06-03 17:31:43 +0100

    cookie-parsar

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ mongoose
 app.use(express.static("FRONTEND"));
 app.use(cors());
 app.use(helmet());
-app.use(cookieParse());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
